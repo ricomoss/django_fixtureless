00a4c9e91c00eea1cc11cbdfb139cf376e7acf6c
Rico Cordova <rico@neutroninteractive.com>
2013-12-12 11:56:16 -0700

    Organizing PiPY content for release.

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ LONG_DESCRIPTION = """
 django-fixtureless
 =====================================
 
-fixtureless provides an easy way to create model objects within your Django
-project for testing purposes.  This is done through inspection of your
-database relationships.  All required fields are randomly filled.
-All foreign key relationships will create respective objects in the
-same fashion.
+django-fixtureless provides an easy way to create model objects within
+your Django project for testing purposes.  This is done through
+inspection of your database relationships.  All required fields are
+randomly filled.  All foreign key relationships will create respective
+objects in the same fashion.
 
 .. note:: django-fixtureless only supports Django 1.4 or higher and Python
 2.7.x and Python 3.3.x.
@@ -26,9 +26,8 @@ setup(
     author_email='rico.cordova@rocksolidbox.com',
     packages=['fixtureless'],
     url='https://github.com/ricomoss/django-fixtureless',
-    license='LICENSE',
+    license='LICENSE.txt',
     description='fixtureless testing utility',
     long_description=LONG_DESCRIPTION,
-    keywords='unittest,django',
-    install_requires=['django >= 1.4'],
+    keywords=['unittest', 'django'],
 )
