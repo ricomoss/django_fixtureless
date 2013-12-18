f43f6978cff6940c25ff856311434846a6852c0e
Rico Cordova <rico@neutroninteractive.com>
2013-12-18 07:52:47 -0700

    PEP8 fix due to typo.

--- fixtureless/tests/test_django_project/test_django_project/settings/postgres.py
+++ fixtureless/tests/test_django_project/test_django_project/settings/postgres.py
@@ -4,7 +4,7 @@ from settings.base import *
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES ['default'] = {
+DATABASES['default'] = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'test_django_project_db',
     'USER': 'test_user',
